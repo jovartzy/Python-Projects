337cd3a208d118bf5a1b98e3592a40ceb8ad1215
jovartzy <jovartzy@gmail.com>
2022-10-16 03:47:36 -0400

    Finish converter

--- converter.py
+++ converter.py
@@ -1,11 +1,8 @@
 import PySimpleGUI as sg
 layout = [
-    [sg.Text('Enter kg to convert:')],
-    [sg.Input(key='-KG-'), sg.Button('Convert', key='-CONVERTKG-')],
-    [sg.Text(key='-LBS-')],
-    [sg.Text('Enter km to convert:')],
-    [sg.Input(key='-KM-'), sg.Button('Convert', key='-CONVERTKM-')],
-    [sg.Text(key='-MILES-')]
+    [sg.Text('Conversion Program')],
+    [sg.Input(key='-INPUT-'), sg.Spin(['kg to lbs','km to miles'], key='-SPIN-'), sg.Button('Convert', key='-CONV-')],
+    [sg.Text(key='-FINAL-')]
 ]
 
 windows = sg.Window('Converter', layout)
@@ -16,11 +13,21 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    if event == '-CONVERTKG-':
-        windows['-LBS-'].update(str(round(float(values['-KG-']) * 2.20462, 3)) + ' lbs' )
+    # if event == '-CONVERTKG-':
+    #     windows['-LBS-'].update(str(round(float(values['-KG-']) * 2.20462, 3)) + ' lbs' )
     
-    if event == '-CONVERTKM-':
-        windows['-MILES-'].update(str(round(float(values['-KM-']) * 0.6214, 3)) + '  miles')
-        
+    # if event == '-CONVERTKM-':
+    #     windows['-MILES-'].update(str(round(float(values['-KM-']) * 0.6214, 3)) + '  miles')
+
+    if event == '-CONV-':
+        print(values['-INPUT-'])
+        input_vals = values['-INPUT-']
+        if input_vals.isnumeric():
+            match values['-SPIN-']:
+                case 'kg to lbs':
+                    windows['-FINAL-'].update(str(round(float(values['-INPUT-']) * 2.20462, 3)) + ' lbs' )
+                case 'km to miles':
+                    windows['-FINAL-'].update(str(round(float(values['-INPUT-']) * 0.6214, 3)) + '  miles')
+    
 
 windows.close()
